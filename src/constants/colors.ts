b78325db40eb22d4b6924051b6b7a3c381541111
asdf <asdf>
2026-05-07 13:02:32 +0530

    color for theme

---
+++ src/constants/colors.ts
@@ -0,0 +1,23 @@
+export const COLORS = {
+  white: '#FFFFFF',
+  black: '#000000',
+  gray: '#808080',
+
+  red: '#FF0000',
+  green: '#00FF00',
+  blue: '#0000FF',
+
+  yellow: '#FFFF00',
+  cyan: '#00FFFF',
+  magenta: '#FF00FF',
+
+  lightGray: '#D3D3D3',
+  darkGray: '#333333',
+  orange: '#FFA500',
+  purple: '#800080',
+  teal: '#008080',
+  navy: '#001F3F',
+
+  splash:'#004B5E'
+};
+export default COLORS;
